c6eba612a7d12667d9a23147aee9b87fb997e40d
SergioCrisostomo <sergiosbox@gmail.com>
2014-03-01 20:26:19 +0100

    Only iPhone

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,126 +9,255 @@ module.exports = function(grunt) {
 
 	require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
-		'connect': {
-			testserver: {
-				options: {
-					// We use end2end task (which does not start the webserver)
-					// and start the webserver as a separate process (in travis_build.sh)
-					// to avoid https://github.com/joyent/libuv/issues/826
-					port: 8000,
-					hostname: '0.0.0.0',
-					middleware: function(connect, options) {
-						return [
-							function(req, resp, next) {
-								// cache get requests to speed up tests on travis
-								if (req.method === 'GET') {
-									resp.setHeader('Cache-control', 'public, max-age=3600');
-								}
+        grunt.initConfig({
+            'connect': {
+                testserver: {
+                    options: {
+                        // We use end2end task (which does not start the webserver)
+                        // and start the webserver as a separate process (in travis_build.sh)
+                        // to avoid https://github.com/joyent/libuv/issues/826
+                        port: 8000,
+                        hostname: '0.0.0.0',
+                        middleware: function(connect, options) {
+                            return [
+                                function(req, resp, next) {
+                                    // cache get requests to speed up tests on travis
+                                    if (req.method === 'GET') {
+                                        resp.setHeader('Cache-control', 'public, max-age=3600');
+                                    }
 
-								next();
-							},
-							connect.static(options.base)
-						];
-					}
-				}
-			}
-		},
+                                    next();
+                                },
+                                connect.static(options.base)
+                            ];
+                        }
+                    }
+                }
+            },
 
-		'packager': {
+            'packager': {
 
-			options: {
-				name: 'Core'
-			},
+                options: {
+                    name: 'Core'
+                },
 
-			all: {
-				src: 'Source/**/*.js',
-				dest: 'mootools-all.js'
-			},
+                all: {
+                    src: 'Source/**/*.js',
+                    dest: 'mootools-all.js'
+                },
 
-			nocompat: {
-				options: {
-					strip: ['.*compat'],
-					only: '<%= grunt.option("file") && "Core/" + grunt.option("file") %>'
-				},
-				src: 'Source/**/*.js',
-				dest: 'mootools-nocompat.js'
-			},
+                nocompat: {
+                    options: {
+                        strip: ['.*compat'],
+                        only: '<%= grunt.option("file") && "Core/" + grunt.option("file") %>'
+                    },
+                    src: 'Source/**/*.js',
+                    dest: 'mootools-nocompat.js'
+                },
 
-			specs: {
-				options: {
-					name: 'Specs'
-				},
-				src: 'Specs/<%= grunt.option("module") || "**" %>/<%= grunt.option("file") || "*" %>.js',
-				dest: 'mootools-specs.js'
-			},
+                specs: {
+                    options: {
+                        name: 'Specs'
+                    },
+                    src: 'Specs/<%= grunt.option("module") || "**" %>/<%= grunt.option("file") || "*" %>.js',
+                    dest: 'mootools-specs.js'
+                },
 
-			'specs-nocompat': {
-				options: {
-					name: 'Specs',
-					strip: ['.*compat'],
-					only: '<%= grunt.option("file") && "Specs/" + grunt.option("file") %>'
-				},
-				src: 'Specs/**/*.js',
-				dest: 'mootools-specs.js'
-			}
+                'specs-nocompat': {
+                    options: {
+                        name: 'Specs',
+                        strip: ['.*compat'],
+                        only: '<%= grunt.option("file") && "Specs/" + grunt.option("file") %>'
+                    },
+                    src: 'Specs/**/*.js',
+                    dest: 'mootools-specs.js'
+                }
 
-		},
+            },
 
-		'karma': {
+            'karma': {
 
-			options: {
-				frameworks: ['jasmine', 'sinon'],
-				files: ['http://rawgithub.com/bitovi/legacy-syn/master/dist/syn.js', 'mootools-*.js'],
-				sauceLabs: {
-					username: process.env.SAUCE_USERNAME,
-					accessKey: process.env.SAUCE_ACCESS_KEY,
-					testName: 'MooTools-Core'
-				},
-				customLaunchers: {
-					chrome_linux: {
-						base: 'SauceLabs',
-						browserName: 'chrome',
-						platform: 'linux'
-					},
-					firefox_linux: {
-						base: 'SauceLabs',
-						browserName: 'firefox',
-						platform: 'linux'
-					},
-					opera_win2000: {
-						base: 'SauceLabs',
-						browserName: 'opera',
-				        platform: 'Windows 2008',
-				        version: '12'
-					}
-				},
-			},
+                options: {
+                    frameworks: ['jasmine', 'sinon'],
+                    files: ['http://rawgithub.com/bitovi/legacy-syn/master/dist/syn.js', 'mootools-*.js'],
+                    sauceLabs: {
+                        username: process.env.SAUCE_USERNAME,
+                        accessKey: process.env.SAUCE_ACCESS_KEY,
+                        testName: 'MooTools-Core'
+                    },
+                    customLaunchers: {/*
+                        chrome_linux: {
+                            base: 'SauceLabs',
+                            browserName: 'chrome',
+                            platform: 'linux'
+                        },
+                        firefox_linux: {
+                            base: 'SauceLabs',
+                            browserName: 'firefox',
+                            platform: 'linux'
+                        },
+                        opera_win2000: {
+                            base: 'SauceLabs',
+                            browserName: 'opera',
+                            platform: 'Windows 2008',
+                            version: '12'
+                        },
+                        safari7: {
+                            base: 'SauceLabs',
+                            browserName: 'safari',
+                            platform: 'OS X 10.9',
+                            version: '7'
+                        },
+                        safari6: {
+                            base: 'SauceLabs',
+                            browserName: 'safari',
+                            platform: 'OS X 10.8',
+                            version: '6'
+                        },
+                        safari5_osx10_6: {
+                            base: 'SauceLabs',
+                            browserName: 'safari',
+                            version: '5',
+                            platform: 'OS X 10.6'
+                        },
+                        safari5_win7: {
+                            base: 'SauceLabs',
+                            browserName: 'safari',
+                            version: '5',
+                            platform: 'Windows 7'
+                        },
+                        ie11: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '11'
+                        },
+                        ie10: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '10'
+                        },
+                        ie9: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '9'
+                        },
+                        ie8: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows XP',
+                            version: '8'
+                        },
+                        ie7: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows XP',
+                            version: '7'
+                        },
+                        ie6: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows XP',
+                            version: '6'
+                        },*/
+                        iphone_7: {
+                            base: 'SauceLabs',
+                            browserName: 'iphone',
+                            platform: 'OS X 10.9',
+                            version: '7'
+                        },
+                        iphone_6_1: {
+                            base: 'SauceLabs',
+                            browserName: 'iphone',
+                            platform: 'OS X 10.8',
+                            version: '6.1'
+                        },
+                        iphone_6: {
+                            base: 'SauceLabs',
+                            browserName: 'iphone',
+                            platform: 'OS X 10.8',
+                            version: '6.0'
+                        }
+                    },
+                },
 
-			continuous: {
-				singleRun: true,
-				browsers: ['PhantomJS']
-			},
+                continuous: {
+                    singleRun: true,
+                    browsers: ['PhantomJS']
+                },
+/*
+                sauce1: {
+                    singleRun: true,
+                    browsers: [
+                        'chrome_linux', 
+                        'firefox_linux', 
+                        'opera_win2000'
+                    ]
+                },
 
-			sauce1: {
-				singleRun: true,
-				browsers: [
-					'chrome_linux', 
-					'firefox_linux', 
-					'opera_win2000'
-				]
-			}
+               sauce2: {
+                    singleRun: true,
+                    browsers: [
+                        'safari7',
+                        'safari6'
+                    ],
+                },
 
-		},
+                sauce3: {
+                    singleRun: true,
+                    browsers: [
+                        'safari5_win7',
+                        'safari5_osx10_6'/*,
+                        
+                    ]
+                },
 
-		'clean': {
-			all: {
-				src: 'mootools-*.js'
-			}
-		}
-	});
+                sauce4: {
+                    singleRun: true,
+                    browsers: [
+                        'ie9',
+                        'ie8',
+                        'ie7'
+                    ]
+                },
+
+                sauce5: {
+                    singleRun: true,
+                    browsers: [
+                        'ie6',
+                        'ie11',
+                        'ie10'
+                       
+                    ]
+                },*/
+
+                sauce6: {
+                    singleRun: true,
+                    browsers: [
+                        'iphone_6',
+                        'iphone_7',
+                        'iphone_6_1'
+                    ]
+                },
+
+                dev: {
+                    browsers: ['PhantomJS'],
+                    reporters: 'dots'
+                }
+
+            },
+
+            'clean': {
+                all: {
+                    src: 'mootools-*.js'
+                }
+            }
+
+        });
 
 	grunt.registerTask('default', ['clean', 'packager:all', 'packager:specs', 'karma:continuous']);
 	grunt.registerTask('nocompat', ['clean', 'packager:nocompat', 'packager:specs-nocompat', 'karma:continuous']);
-	grunt.registerTask('default:travis', ['clean', 'packager:all', 'packager:specs', 'karma:sauce1'/*, 'karma:sauce2', 'karma:sauce3', 'karma:sauce4', 'karma:sauce5', 'karma:sauce6'*/])
+	grunt.registerTask('default:travis', ['clean', 'packager:all', 'packager:specs', /*'karma:sauce1', 'karma:sauce2', 'karma:sauce3', 'karma:sauce4', 'karma:sauce5',*/ 'karma:sauce6'])
 };
