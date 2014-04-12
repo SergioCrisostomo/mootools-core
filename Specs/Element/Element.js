fb2586cfac8b44598089bef681edae964b557977
SergioCrisostomo <sergiosbox@gmail.com>
2014-04-12 10:36:51 +0200

    Add new spec

--- Specs/Element/Element.js
+++ Specs/Element/Element.js
@@ -2497,3 +2497,12 @@ describe('new Element(expression)', function(){
 	});
 
 });
+
+describe('creating new Elements', function(){
+
+	it('should create an element with type="email"', function(){
+		var el = new Element('input', {type: 'email'});
+		expect(el.get('type')).toMatch(/email|text/);
+	});
+
+});
